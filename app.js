b337c971bf4343ad7201af610bf6dbce468c28e6
Carolina Neponucena <cneponucena@gmail.com>
2023-09-20 16:18:17 -0300

    pronto

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const cepInput = document.getElementById('cep')
+
+
+async function pegarCep (cep) {
+
+    const url = `https://viacep.com.br/ws/${cep}/json/`
+    const response = await fetch(url)
+    const cepInfo = await response.json()
+    return cepInfos
+}
+
+async function preencherCampos () {
+    const endereco = document.getElementById('endereco')
+    const bairro = document.getElementById('bairro')
+    const cidade = document.getElementById('cidade')
+    const estado = document.getElementById('estado')
+
+    const cep = await pegarCep(cepInput.value)
+    
+    endereco.value = cep.logradouro
+    bairro.value = cep.bairro
+    cidade.value = cep.localidade
+    estado.value = cep.uf
+
+}
+
+
+cepInput.addEventListener('focusout', preencherCampos)
